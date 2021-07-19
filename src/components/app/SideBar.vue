a093c1b324415a654d46adc2e1178e54386ad149
dmitry <dmytro.bolhov@gmail.com>
2021-07-19 14:33:16 +0300

    update files

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--  <ul class="sidenav app-sidenav open">-->
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+    <li v-for="link in links" :key="link.url">
+      <router-link
+          active-class="active"
+          :to="link.url"
+          :exact="link.exact"
+          class="waves-effect waves-orange pointer"
+      >
+        {{ link.title }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['isOpen'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/', exact: true},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категории', url: '/categories'}
+    ]
+  })
+}
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,65 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Home',
+        meta: {layout: 'main'},
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        meta: {layout: 'empty'},
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/categories',
+        name: 'categories',
+        meta: {layout: 'main'},
+        component: () => import('../views/Categories.vue')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        meta: {layout: 'empty'},
+        component: () => import('../views/Register.vue')
+    },
+    {
+        path: '/detail-record',
+        name: 'detail-record',
+        meta: {layout: 'main'},
+        component: () => import('../views/DetailRecord.vue')
+    },
+    {
+        path: '/history',
+        name: 'history',
+        meta: {layout: 'main'},
+        component: () => import('../views/History.vue')
+    },
+    {
+        path: '/planning',
+        name: 'planning',
+        meta: {layout: 'main'},
+        component: () => import('../views/Planning.vue')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        meta: {layout: 'main'},
+        component: () => import('../views/Profile.vue')
+    },
+    {
+        path: '/record',
+        name: 'record',
+        meta: {layout: 'main'},
+        component: () => import('../views/Record.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
